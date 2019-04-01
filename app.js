a6aeaee975382b892998b2bae9667fd54025e598
Mario Lema <lema_mario@yahoo.com>
2019-04-01 15:20:15 +0000

    finish functionality

--- app.js
+++ app.js
@@ -4,16 +4,36 @@ const DATA = {
     currPair: [],
     moves: 0,
     pairsSolved: 0,
-    gameOngoing: false
+    gameOngoing: false,
+    busy: false,
+    timeLeft: 100,
+    interval: setInterval( () =>  MODIFIER.countdown() , 1000)
 };
 const VIEW = {
 
-    displaySolvedPairs() {
-        document.getElementById(`matches`).innerHTML = `MATCHES: ${DATA.pairsSolved}`;
+    displayTurns() {
+        document.getElementById(`turns`).innerHTML = `TRIES: ${DATA.moves}`;
     },
 
-    displayTimeLeft(seconds) {
-        document.getElementById(`time-left`).innerHTML = `TIME LEFT: ${seconds}`;
+    hideEnd(){
+        let endMessage = document.getElementById(`end-message`);
+        endMessage.style.display = `none`;
+    },
+
+    displayEnd(winLose){
+        setTimeout( () => {
+            let endMessage = document.getElementById(`end-message`);
+            endMessage.style.display = `flex`;
+            if(winLose === `win`){
+            endMessage.firstElementChild.innerHTML = `YOU WIN!`;
+            }else{
+            endMessage.firstElementChild.innerHTML = `OUT OF TIME`;
+            }
+        }, 1000)
+
+    },
+    displayTimeLeft() {
+        document.getElementById(`time-left`).innerHTML = `TIME LEFT: ${DATA.timeLeft}s`;
     },
 
     revealCard(id) {
@@ -27,19 +47,38 @@ const VIEW = {
     addFace(arrId, faceId) {
         document.getElementById(`avatar-${arrId[0]}`).innerHTML = `<img class="face-image" src="images/face-${faceId}.png" alt=""></img>`;
         document.getElementById(`avatar-${arrId[1]}`).innerHTML = `<img class="face-image" src="images/face-${faceId}.png" alt=""></img>`;
+    },
+
+    winkUp() {
+        event.target.firstElementChild.src = `images/open-eye.png`;
+    },
+    winkDown() {
+        event.target.firstElementChild.src = `images/closed-eye.png`;
+    },
+
+    alertStart(){ 
+        document.getElementById(`reminder`).setAttribute("style", "color:red; font-size: 1.5rem;");
     }
 };
 const MODIFIER = {
 
     turn() {
+        if(!DATA.gameOngoing) VIEW.alertStart();
         let id = this.correctTarget(event);
-        if (id && DATA.gameOngoing) {
+        if (id && DATA.gameOngoing && !DATA.busy) {
             DATA.currPair.push(Number(id));
             VIEW.revealCard(id);
+            DATA.busy = true;
+            setTimeout( () => DATA.busy = false, 500);
             if (DATA.currPair.length > 1) {
                 DATA.moves++;
+                VIEW.displayTurns();
                 if (this.checkMatch()) {
-                    DATA.pairsSolved++
+                    DATA.pairsSolved++;
+                    if(this.checkWin()) {
+                        VIEW.displayEnd(`win`);
+                        clearInterval(DATA.interval);
+                    };
                     DATA.currPair = [];
                 } else {
                     setTimeout(() => {
@@ -55,12 +94,33 @@ const MODIFIER = {
 
 
     },
+    countdown(){
+        if(DATA.gameOngoing){
+            VIEW.displayTimeLeft();
+            DATA.timeLeft--;
+            if(DATA.timeLeft === 0){
+                VIEW.displayTimeLeft();
+                clearInterval(DATA.interval);
+                if(this.checkWin()){
+                    VIEW.displayEnd(`win`);
+                }else{
+                    VIEW.displayEnd(`lose`);
+                }
+            }
+        }
+    },
+
+
+    checkWin(){
+        return DATA.pairsSolved === 8 ? true : false;
+    },
 
     checkMatch() {
         for (let i = 0; i < DATA.matchTable.length; i++) {
             let firstMatch = DATA.matchTable[i].positions.indexOf(DATA.currPair[0]) !== -1 ? true : false;
             let secondMatch = DATA.matchTable[i].positions.indexOf(DATA.currPair[1]) !== -1 ? true : false;
             if (firstMatch && secondMatch) {
+                DATA.matchTable.splice(i, 1);
                 return true;
             }
         }
@@ -79,16 +139,51 @@ const MODIFIER = {
     },
 
     startGame() {
-        DATA.gameOngoing = true;
-        this.generateMatches()
-
-        for (let i = 0; i < DATA.matchTable.length; i++) {
-            VIEW.addFace(DATA.matchTable[i].positions, DATA.matchTable[i].faceId);
+        if(!DATA.gameOngoing){
+            this.generateMatches()
+    
+            for (let i = 0; i < DATA.matchTable.length; i++) {
+                VIEW.addFace(DATA.matchTable[i].positions, DATA.matchTable[i].faceId);
+            }
+            this.revealAll();
         }
+
+    },
+
+    resetGame(){
+        DATA.boardArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+        DATA.matchTable = [];
+        DATA.currPair = [];
+        DATA.moves = 0;
+        DATA.pairsSolved = 0;
+        DATA.gameOngoing = false;
+        DATA.busy = false;
+        DATA.timeLeft = 100;
+
+      VIEW.displayTimeLeft();
+      VIEW.displayTurns();
+      VIEW.hideEnd();
+
+      for(let i = 0; i < DATA.boardArr.length; i++){
+          VIEW.hideCard(i);
+      }
+
+    },
+
+    revealAll(){
+        for(let i = 0; i < DATA.boardArr.length; i++){
+            VIEW.revealCard(i);
+        }
+        setTimeout( () => {
+            for(let i = 0; i < DATA.boardArr.length; i++){
+                VIEW.hideCard(i);
+            }
+            DATA.gameOngoing = true;
+        }, 3000 )
     },
 
     generateMatches() {
-        let board = DATA.boardArr;
+        let board = [...DATA.boardArr];
         //Array.from(Array(10).keys())
         let faceBoard = [...Array(66).keys()]
         while (board.length !== 0) {
@@ -101,18 +196,14 @@ const MODIFIER = {
             });
         }
     },
+    
 
-    winkUp() {
-        event.target.firstElementChild.src = `images/open-eye.png`;
-    },
-    winkDown() {
-        event.target.firstElementChild.src = `images/closed-eye.png`;
-    },
+
 
     winkUpListener(nodesArr) {
         for (let i = 0; i < nodesArr.length; i++) {
-            nodesArr[i].addEventListener(`mouseenter`, this.winkUp, false);
-            nodesArr[i].addEventListener(`mouseleave`, this.winkDown, false);
+            nodesArr[i].addEventListener(`mouseenter`, VIEW.winkUp, false);
+            nodesArr[i].addEventListener(`mouseleave`, VIEW.winkDown, false);
         }
     }
 }
@@ -122,114 +213,5 @@ MODIFIER.startGame = MODIFIER.startGame.bind(MODIFIER);
 //========================CLICK EVENTS============================================
 document.getElementById(`memory-game`).addEventListener(`click`, MODIFIER.turn, false);
 document.getElementById(`start-game`).addEventListener(`click`, MODIFIER.startGame, false);
+document.getElementById(`reset-game`).addEventListener(`click`, MODIFIER.resetGame, false);
 MODIFIER.winkUpListener(document.querySelectorAll(`.card-front`));
-
-
-
-
-
-// //========================================================================
-
-// class MixOrMatch {
-//     constructor(totalTime, cards) {
-//         this.cardsArray = cards;
-//         this.totalTime = totalTime;
-//         this.timeRemaining = totalTime;
-//         this.timer = document.getElementById('time-remaining');
-//         this.ticker = document.getElementById('flips');
-//         this.audioController = new AudioController();
-//     }
-
-//     startGame() {
-//         this.totalClicks = 0;
-//         this.timeRemaining = this.totalTime;
-//         this.cardToCheck = null;
-//         this.matchedCards = [];
-//         this.busy = true;
-//         setTimeout(() => {
-//             this.audioController.startMusic();
-//             this.shuffleCards(this.cardsArray);
-//             this.countdown = this.startCountdown();
-//             this.busy = false;
-//         }, 500)
-//         this.hideCards();
-//         this.timer.innerText = this.timeRemaining;
-//         this.ticker.innerText = this.totalClicks;
-//     }
-//     startCountdown() {
-//         return setInterval(() => {
-//             this.timeRemaining--;
-//             this.timer.innerText = this.timeRemaining;
-//             if(this.timeRemaining === 0)
-//                 this.gameOver();
-//         }, 1000);
-//     }
-//     gameOver() {
-//         clearInterval(this.countdown);
-//         this.audioController.gameOver();
-//         document.getElementById('game-over-text').classList.add('visible');
-//     }
-//     victory() {
-//         clearInterval(this.countdown);
-//         this.audioController.victory();
-//         document.getElementById('victory-text').classList.add('visible');
-//     }
-//     hideCards() {
-//         this.cardsArray.forEach(card => {
-//             card.classList.remove('visible');
-//             card.classList.remove('matched');
-//         });
-//     }
-//     flipCard(card) {
-//         if(this.canFlipCard(card)) {
-//             this.audioController.flip();
-//             this.totalClicks++;
-//             this.ticker.innerText = this.totalClicks;
-//             card.classList.add('visible');
-
-//             if(this.cardToCheck) {
-//                 this.checkForCardMatch(card);
-//             } else {
-//                 this.cardToCheck = card;
-//             }
-//         }
-//     }
-//     checkForCardMatch(card) {
-//         if(this.getCardType(card) === this.getCardType(this.cardToCheck))
-//             this.cardMatch(card, this.cardToCheck);
-//         else 
-//             this.cardMismatch(card, this.cardToCheck);
-
-//         this.cardToCheck = null;
-//     }
-//     cardMatch(card1, card2) {
-//         this.matchedCards.push(card1);
-//         this.matchedCards.push(card2);
-//         card1.classList.add('matched');
-//         card2.classList.add('matched');
-//         this.audioController.match();
-//         if(this.matchedCards.length === this.cardsArray.length)
-//             this.victory();
-//     }
-//     cardMismatch(card1, card2) {
-//         this.busy = true;
-//         setTimeout(() => {
-//             card1.classList.remove('visible');
-//             card2.classList.remove('visible');
-//             this.busy = false;
-//         }, 1000);
-//     }
-//     shuffleCards(cardsArray) { // Fisher-Yates Shuffle Algorithm.
-//         for (let i = cardsArray.length - 1; i > 0; i--) {
-//             let randIndex = Math.floor(Math.random() * (i + 1));
-//             cardsArray[randIndex].style.order = i;
-//             cardsArray[i].style.order = randIndex;
-//         }
-//     }
-//     getCardType(card) {
-//         return card.getElementsByClassName('card-value')[0].src;
-//     }
-//     canFlipCard(card) {
-//         return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
-//     }
-// }
